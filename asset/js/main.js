dfd48294a4f37c6b5ab2b4dcd46404a0d112fd8a
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-03-11 15:54:04 +0100

    create element for exercize mail

--- asset/js/main.js
+++ asset/js/main.js
@@ -23,3 +23,29 @@
 // } else {
 //     console.log("crash");
 // }
+
+const emailButton = document.getElementById('email');
+const dadiButton = document.getElementById('dadi');
+const playEmail = document.getElementById('playEmail');
+//const titlePage = document.getElementsByTagName('h1');
+
+
+emailButton.addEventListener('click', function () {
+    // nascondoBottone e titolo
+    dadiButton.classList = 'd-none';
+    //titlePage.classList.add("d-none");
+    //     Mail
+    // Chiedi all’utente la sua email,
+    // controlla che sia nella lista di chi può accedere,
+    // stampa un messaggio appropriato sull’esito del controllo.
+    playEmail.classList.remove('d-none');
+
+
+})
+
+dadiButton.addEventListener('click', function () {
+    // nascondoBottone e titolo
+    emailButton.classList = 'd-none';
+    //titlePage.classList.add("d-none");
+})
+
