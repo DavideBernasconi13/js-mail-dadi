841232f19bc06cbc2f57b0ed1f82960d6732266c
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-03-11 15:22:29 +0100

    init e structure html con css custom

---
+++ asset/js/main.js
@@ -0,0 +1,25 @@
+// let nomi = ["Marco", "Giacomo", "Giovanni"];
+// for (i = 0; i < nomi.length; i++) {
+//     console.log(nomi[i]);
+// }
+
+// let userWord = 'Tastiera';
+// let trovato = false;
+
+// let text = ['tavolo', 'sedia', 'libro', 'mouse'];
+
+// console.log(text);
+
+// for (let i = 0; i < text.length; i++) {
+//     text.push = userWord;
+//     console.log(text[i]);
+//     if (userWord === text[i]) {
+//         trovato = true;
+//     }
+// }
+
+// if (trovato) {
+//     console.log('OK, trovato!')
+// } else {
+//     console.log("crash");
+// }
