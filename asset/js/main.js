eba57755ffc630e21ce720ef34fd33215ee70703
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-03-12 10:37:24 +0100

    esercizio dadi senza immagini

--- asset/js/main.js
+++ asset/js/main.js
@@ -68,7 +68,7 @@ emailButton.addEventListener('click', function () {
 
         for (let i = 0; i < mailAutorizzate.length; i++) {
             console.log(mailAutorizzate[i]);
-            if (valueMail === mailAutorizzate[i]) {
+            if (valueMail.toLowerCase() === mailAutorizzate[i].toLowerCase()) {
                 mailTrovata = true;
             }
         }
@@ -93,10 +93,48 @@ emailButton.addEventListener('click', function () {
 
 })
 
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
 dadiButton.addEventListener('click', function () {
     // nascondoBottone e  mostro bottone per uscire
     emailButton.classList = 'd-none';
     esciButton.classList.remove('d-none');
     dadiButton.classList.add('active');
+
+    let userNumber = document.createElement('div');
+    let pcNumber = document.createElement('div');
+    let container = document.createElement('div');
+    container.setAttribute("class", "container d-flex");
+    pcNumber.setAttribute("class", "text-center");
+    userNumber.setAttribute("class", "text-center");
+    let numeroRandomUser = parseInt(Math.random() * 6 + 1);
+    let numeroRandomPC = parseInt(Math.random() * 6 + 1);
+    userNumber.innerHTML = `Scelta dell'utente: ${numeroRandomUser}`;
+    pcNumber.innerHTML = `Scelta del pc: ${numeroRandomPC}`;
+    // stampa i numeri a schermo
+    document.body.appendChild(container);
+    container.appendChild(userNumber);
+    container.appendChild(pcNumber);
+
+    // genera il div e stampare il risultato
+    let result = document.createElement("div");
+    result.setAttribute("class", "container result");
+    result.innerHTML = '';
+    if (numeroRandomUser === numeroRandomPC) {
+        result.innerHTML = "Hai pareggiato";
+        result.classList.add('game-over');
+    } else if (numeroRandomUser < numeroRandomPC) {
+        result.innerHTML = "Ha vinto il pc";
+        result.classList.add('game-over');
+    } else {
+        result.innerHTML = "Hai vinto tu!";
+        result.classList.add('win');
+    }
+
+    document.body.appendChild(result);
+
 })
 
